c9d7ec821094e422e9e324c28998359929432226
Diogo Wernik <diogowernik@gmail.com>
2016-08-15 13:15:51 -0400

    GET cards from yoga.brasilia.io

--- www/js/controllers/cardsCtrl.js
+++ www/js/controllers/cardsCtrl.js
@@ -1,3 +1,11 @@
 angular.module('yogabrasilia.controllers')
 
-.controller('CardsCtrl', function($scope) {});
+.controller('CardsCtrl', function($scope, $http) {
+
+  var api = 'http://yoga.brasilia.io/cards/from-subdomain/';
+
+  $http.get(api+'1.json').success(function(data, status, headers, config){
+    $scope.cards = data;
+  });
+
+})
--- www/js/controllers/publicationsCtrl.js
+++ www/js/controllers/publicationsCtrl.js
@@ -1,3 +1,11 @@
 angular.module('yogabrasilia.controllers')
 
-.controller('PublicationsCtrl', function($scope) {});
+.controller('PublicationsCtrl', function($scope, $http) {
+
+  var api = 'http://yoga.brasilia.io/publications/from-subdomain/';
+
+  $http.get(api+'1.json').success(function(data, status, headers, config){
+    $scope.cards = data;
+  });
+  
+})
